58e48d5119a8b9218e76c8aa6f2785c49338c47f
DarkXz7 <miguelangelok09@hotmail.com>
2024-10-19 12:47:53 -0500

    commits

---
+++ LandingPage Producto/prueba.js
@@ -0,0 +1,11 @@
+let miNumero=10;
+
+if (miNumero > 0){
+    console.log(`numero positivo ${miNumero}`)
+}
+else if (miNumero<0){
+    console.log(`Valor negativo ${miNumero}`)
+}
+else{
+    console.log(`numero negativo ${miNumero}`)
+}
